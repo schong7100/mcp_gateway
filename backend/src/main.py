4de025478e6b666f1fdb2cb2aeff574eb52ed7be
schong <schong7100@gmail.com>
2026-04-02 16:10:17 +0900

    fix: restore VM IP in CORS allow_origins

--- backend/src/main.py
+++ backend/src/main.py
@@ -40,7 +40,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://10.1.10.188:13000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
